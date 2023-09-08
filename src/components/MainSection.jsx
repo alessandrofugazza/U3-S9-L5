f5368a2d4fb0b05441150a1859263983ab1fad97
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2023-09-08 15:40:41 +0200

    improvements

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -12,15 +12,19 @@ class MainSection extends Component {
     this.setState({ isLoading: true });
     try {
       const seriesName = this.props.seriesName;
-      const apiUrl = "http://www.omdbapi.com/?apikey=7cf04ae9";
+      const apiUrl = "http://www.omdbapi.com/?apikey=7cf04ae9&type=movie";
       const searchUrl = "&s=" + seriesName;
       const fetchUrl = apiUrl + searchUrl;
       const re = await fetch(fetchUrl);
       if (re.ok) {
         const allMovies = await re.json();
         const movies = [];
-        for (let i = 0; i < 6; i++) {
-          movies.push(allMovies.Search[i]);
+        if (allMovies.Search) {
+          const moviesFound = allMovies.Search.length;
+          console.log(moviesFound);
+          for (let i = 0; i < (moviesFound < 6 ? moviesFound : 6); i++) {
+            movies.push(allMovies.Search[i]);
+          }
         }
         this.setState({ movies });
       } else {
@@ -39,16 +43,24 @@ class MainSection extends Component {
   render() {
     return (
       <section>
-        {this.state.hasError && <Alert variant="danger">Fetch error</Alert>}
-        {this.state.isLoading && <Spinner animation="border" variant="warning" />}
-        {this.state.movies.length === 0 && !this.state.hasError && !this.state.isLoading && <p>No movies found.</p>}
         <h4 style={{ fontSize: "1.5rem" }}>{this.props.seriesName}</h4>
+        {/* --------------------------------------------------------------------------- */}
+        {/* Usare Alert qui mi da un errore per qualche motivo! */}
+        {/* {this.state.hasError && <Alert variant="danger">Fetch error</Alert>} */}
+        {/* --------------------------------------------------------------------------- */}
+        {this.state.hasError && <p>Fetch error</p>}
+        {this.state.isLoading && <Spinner animation="border" style={{ color: "#E50914" }} />}
+        {this.state.movies.length === 0 && !this.state.hasError && !this.state.isLoading && <p>No movies found.</p>}
         <Container fluid>
           <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 mb-4 no-gutters text-center">
             {this.state.movies.map(movie => {
               return (
                 <div className={"col mb-2 px-1 d-flex"} key={movie.imdbID}>
-                  <img className="img-fluid object-fit-cover" src={movie.Poster} alt="movie picture" />
+                  {movie.Poster !== "N/A" ? (
+                    <img className="img-fluid object-fit-cover" src={movie.Poster} alt="movie picture" />
+                  ) : (
+                    <p className="h6 my-auto ">{movie.Title}</p>
+                  )}
                 </div>
               );
             })}
